637f471f643b18acb94a862a219190e0860bf400
Alina <Alina@DESKTOP-NB81HVC>
2022-11-15 18:56:49 +0200

    Lab4

--- Pages/Books/Index.cshtml.cs
+++ Pages/Books/Index.cshtml.cs
@@ -30,21 +30,29 @@ namespace Herteg_Alina_Laboratorul2.Pages.Books
         public string CurrentFilter { get; set; }
 
         public string BookCategories { get; set; }
-        public async Task OnGetAsync(int? id, int? categoryID, string sortOrder)
+        public async Task OnGetAsync(int? id, int? categoryID, string sortOrder, string searchString)
         {
             BookD = new BookData();
             TitleSort = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
             AuthorSort = String.IsNullOrEmpty(sortOrder) ? "author_desc" : "";
-     
+
+            CurrentFilter = searchString;
 
         BookD.Books = await _context.Book
-                .Include(a => a.Author)
+                .Include(b => b.Author)
             .Include(b => b.Publisher)
             .Include(b => b.BookCategories)
             .ThenInclude(b => b.Category)
             .AsNoTracking()
             .OrderBy(b => b.Title)
             .ToListAsync();
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                BookD.Books = BookD.Books.Where(s => s.Author.FirstName.Contains(searchString)
+                                      || s.Author.LastName.Contains(searchString)
+                                      || s.Title.Contains(searchString));
+            }
             if (id != null)
             {
                 BookID = id.Value;
